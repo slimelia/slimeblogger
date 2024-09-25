2a7c387ac2dbd9d9570b5fa97f7ed0e965290eee
Alice Lia Stapleton <slimelia@protonmail.com>
2024-09-25 10:34:17 +0100

    Encapsulation changes

--- build/blog_attributes.py
+++ build/blog_attributes.py
@@ -14,25 +14,25 @@ class BlogAttributes:
     """
 
     def __init__(self, string_to_split: str):
-        self.title: str = self.parse_title(string_to_split)
-        self.author: str = self.parse_author(string_to_split)
-        self.date: datetime = self.parse_date(string_to_split)
+        self._title: str = self._parse_title(string_to_split)
+        self._author: str = self._parse_author(string_to_split)
+        self._date: datetime = self._parse_date(string_to_split)
 
-    def parse_title(self, string_to_search: str) -> str:
+    def _parse_title(self, string_to_search: str) -> str:
         """Returns Title as denoted by @title tag.
         """
         if title := re.search(r'(?<=@title ).*?(?=\s@|$)', string_to_search):
             return title.group()
         raise ValueError("@title field missing in one or more blog posts.")
 
-    def parse_author(self, string_to_search: str) -> str:
+    def _parse_author(self, string_to_search: str) -> str:
         """Returns Author as denoted by @author tag.
         """
         if author := re.search(r'(?<=@author ).*?(?=\s@|$)', string_to_search):
             return author.group()
         raise ValueError("@author field missing in one or more blog posts.")
 
-    def parse_date(self, string_to_search: str) -> datetime:
+    def _parse_date(self, string_to_search: str) -> datetime:
         """Returns date as denoted by @date tag.
         """
         if date := re.search(r'(?<=@date )[0-9]{4}-[0-9]{2}-[0-9]{2}'
@@ -40,6 +40,24 @@ class BlogAttributes:
                              string_to_search):
             return datetime.fromisoformat(date.group())
         raise ValueError("@date field missing in one or more blog posts.")
+
+    @property
+    def title(self) -> str:
+        """Get title
+        """
+        return self._title
+
+    @property
+    def author(self) -> str:
+        """Get author
+        """
+        return self._author
+
+    @property
+    def date(self) -> datetime:
+        """Get date
+        """
+        return self._date
 
 
 if __name__ == "__main__":
